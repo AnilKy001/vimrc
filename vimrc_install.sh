378bb4de4e5e6c080c80c35c85691e82afa65265
Anil Kaya <anil01kaya@gmail.com>
2021-02-18 11:28:36 +0300

    shell script fix

--- vimrc_install.sh
+++ vimrc_install.sh
@@ -3,11 +3,11 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 git clone https://github.com/AnilKy001/vimrc ~/.vimrc_minimal
-cat ~/.vimrc_minimal/my_vimrc.vim > ~/.vimrc
+cat ~/.vimrc_minimal/.my_vimrc > ~/.vimrc
 source ~/.vimrc
 
 cd ~/.vim/plugged/YouCompleteMe
 python3 install.py --clangd-completer
 
 echo "Minimal vimrc installation is successfull!"
-echo "Don2t forget to run ':PlugInstall' when you first open vim."
+echo "Don't forget to run ':PlugInstall' when you first open vim."
