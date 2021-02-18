b4b2c15557a7eaf7c39946002aacf717a6b75cfc
AnilKy001 <73109165+AnilKy001@users.noreply.github.com>
2021-02-18 11:50:10 +0300

    Update vimrc_install.sh

--- vimrc_install.sh
+++ vimrc_install.sh
@@ -3,8 +3,8 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cat ./.my_vimrc > ~/.vimrc
+vim +'PlugInstall --sync' +qa
 source ~/.vimrc
-vim -c 'PlugInstall'
 
 cd ~/.vim/plugged/YouCompleteMe
 python3 install.py --clangd-completer
